18af114cb82f1f71ba4449c6fe21d781983b7b0a
Свинухов константин <lopata216@yandex.ru>
2024-11-04 15:07:31 +0300

    1

---
+++ src/main/java/com/example/Book_onlineShop/BookOnlineShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.Book_onlineShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookOnlineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookOnlineShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Book_onlineShop/BookOnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Book_onlineShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookOnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
